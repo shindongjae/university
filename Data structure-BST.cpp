4c899b1f8d1f8dbb3a2d440e1f234b6cb2ee939d
shindongjae <77845710+shindongjae@users.noreply.github.com>
2021-07-11 22:35:24 +0900

    Update Data structure-BST.cpp

--- Data structure-BST.cpp
+++ Data structure-BST.cpp
@@ -29,7 +29,7 @@ public:
 		height = 0;
 	}
 
-	~BST() { this->treeDestructor(this->root);}
+	~BST() { this->treeDestructor(this->root); }
 
 	void treeDestructor(Node* root) {
 		if (root == nullptr)
@@ -87,15 +87,15 @@ public:
 		}
 	}
 
-	void search(int data) {
+	Node* search(int data) {
 		int depth = 0;
 		Node* tmp = root;
 		while (1) {
 			if (data < tmp->data)
-			{	
+			{
 				if (tmp->lchi == nullptr) {
 					cout << 0 << endl;
-					return;
+					return nullptr;
 				}
 				tmp = tmp->lchi;
 				depth++;
@@ -103,7 +103,7 @@ public:
 			else if (data > tmp->data) {
 				if (tmp->rchi == nullptr) {
 					cout << 0 << endl;
-					return;
+					return nullptr;
 				}
 				tmp = tmp->rchi;
 				depth++;
@@ -114,18 +114,21 @@ public:
 					child++;
 				if (tmp->rchi != nullptr)
 					child++;
-				cout << child << ' '<< depth<<endl;
-				return;
+
+				return tmp;
 			}
 		}
 	}
 
-	void setheight(Node *node,int h) {
+
+	void setheight(Node* node, int h) {
 		if (node->lchi != nullptr) {
-			setheight(node->lchi, h + 1);
+			node = node->lchi;
+			setheight(node, h + 1);
 		}
 		if (node->rchi != nullptr) {
-			setheight(node->rchi, h + 1);
+			node = node->rchi;
+			setheight(node, h + 1);
 		}
 		if (node->lchi == nullptr && node->rchi == nullptr) {
 			if (h > height)
@@ -133,21 +136,68 @@ public:
 			return;
 		}
 	}
+
+	void preorder(Node* node) {
+		cout << node->data << ' ';
+		if (node->lchi != nullptr)
+			preorder(node->lchi);
+		if (node->rchi != nullptr)
+			preorder(node->rchi);
+	}
+
 };
+
+void Transplant(BST* t, Node* u, Node* v) {
+	if (u->par == nullptr)
+		t->root = v;
+	else if (u == u->par->lchi)
+		u->par->lchi = v;
+	else
+		u->par->rchi = v;
+	if (v != nullptr)
+		v->par = u->par;
+}
+
+void Delete(BST* t, Node* z) {
+	if (z->lchi == nullptr)
+		Transplant(t, z, z->rchi);
+	else if (z->rchi == nullptr)
+		Transplant(t, z, z->lchi);
+	else {
+		Node* y = z->rchi;
+		while (y->lchi != nullptr)
+			y = y->lchi;
+		if (y->par != z) {
+			Transplant(t, y, y->rchi);
+			y->rchi = z->rchi;
+			y->rchi->par = y;
+		}
+		Transplant(t, z, y);
+		y->lchi = z->lchi;
+		y->lchi->par = y;
+	}
+	delete z;
+}
 
 int main() {
 	int T;
 	cin >> T;
 	while (T--) {
 		BST* bst = new BST;
-		int N, t;
+		int N, t, M;
 		cin >> N;
 		for (int i = 0;i < N;i++) {
 			cin >> t;
 			bst->insertNode(t);
 		}
-		bst->setheight(bst->root,0);
-		cout << bst->height << endl;
+		cin >> M;
+		for (int i = 0;i < M;i++) {
+			cin >> t;
+			Delete(bst, bst->search(t));
+		}
+
+		bst->preorder(bst->root);
+		cout << endl;
 		delete bst;
 	}
 }
